5d2a05efd9e0729ff4313b19bef4f78c1673a46c
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-03-19 20:28:34 +0100

    chore(api): init spring-boot project

---
+++ api/src/main/java/fr/inpt/frappe/FrappeApplication.java
@@ -0,0 +1,13 @@
+package fr.inpt.frappe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrappeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrappeApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/fr/inpt/frappe/FrappeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.inpt.frappe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrappeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
