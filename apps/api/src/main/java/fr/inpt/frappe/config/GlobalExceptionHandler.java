8788008fbb8b587992a7e4f38432a94175338c68
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-03-30 16:59:23 +0200

    chore(spring): force `application/json` response

---
+++ apps/api/src/main/java/fr/inpt/frappe/config/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package fr.inpt.frappe.config;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Map<String, String>> handleException(Exception e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(Map.of("error", e.getMessage()));
+	}
+}
