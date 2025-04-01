82d1ca1f69ae5c9c4143a9fb39c673b6baa28fab
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-04-01 15:06:28 +0200

    feat(api/controllers/school): use a dto

---
+++ apps/api/src/main/java/fr/inpt/frappe/controllers/dtos/SchoolDTO.java
@@ -0,0 +1,15 @@
+package fr.inpt.frappe.controllers.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SchoolDTO {
+	private String uid;
+	private String name;
+}
