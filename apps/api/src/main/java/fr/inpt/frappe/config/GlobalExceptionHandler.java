bdb397dac34306ee1d982f8a4d177d7ed344e195
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-04-04 10:40:40 +0200

    chore(api): support for validation

--- apps/api/src/main/java/fr/inpt/frappe/config/GlobalExceptionHandler.java
+++ apps/api/src/main/java/fr/inpt/frappe/config/GlobalExceptionHandler.java
@@ -1,6 +1,12 @@
 package fr.inpt.frappe.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,5 +16,14 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(ResponseStatusException.class)
 	public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
 		return new ResponseEntity<>(ex.getReason(), ex.getStatusCode());
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+			errors.put(error.getField(), error.getDefaultMessage());
+		}
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 	}
 }
