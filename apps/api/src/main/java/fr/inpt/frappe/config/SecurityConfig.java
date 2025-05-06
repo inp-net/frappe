a5c112d46ec68f056ca1afefda2941d0552e763e
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-05-06 13:18:49 +0200

    chore(api): configure cors

--- apps/api/src/main/java/fr/inpt/frappe/config/SecurityConfig.java
+++ apps/api/src/main/java/fr/inpt/frappe/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+				.cors(cors -> cors.configure(http))
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(
 						sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
