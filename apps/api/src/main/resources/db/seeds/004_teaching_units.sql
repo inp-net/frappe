c6c9f37f0cf13c580bdbb0cdbb3a1a6d8281d22c
Antoine Rey <antoine.rey@etu.inp-n7.fr>
2025-05-06 12:17:12 +0200

    fix(api): make the migration for the modifications of teaching units

--- apps/api/src/main/resources/db/seeds/004_teaching_units.sql
+++ apps/api/src/main/resources/db/seeds/004_teaching_units.sql
@@ -1,7 +1,7 @@
 DO $$ 
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM teaching_units) THEN -- Check if the table is empty
-        INSERT INTO teaching_units (name, minor_id) VALUES 
-            ('Science des Réseaux et Apprentissage', 1);
+        INSERT INTO teaching_units (name) VALUES 
+            ('Science des Réseaux et Apprentissage');
     END IF;
 END $$;
