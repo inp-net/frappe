04f0cf731b7c1562cd1548811d15f7935e8447e6
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-05-06 15:13:04 +0200

    feat(app): generate openapi client + authenticate client

--- apps/app/src/hooks.server.ts
+++ apps/app/src/hooks.server.ts
@@ -1,8 +1,18 @@
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
 import { getAuthUserFromJWT } from './lib/utils';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.me = getAuthUserFromJWT(event.cookies.get('token'));
 	event.locals.authenticated = Boolean(event.locals.me)
 	return await resolve(event);
+};
+
+
+export const handleFetch: HandleFetch = async ({event, request, fetch}) => {
+	const token = event.cookies.get('token');
+
+	if (token)
+		request.headers.set("Authorization", `Bearer ${token}`);
+
+	return fetch(request);
 };
---
+++ apps/app/src/lib/api/client.ts
@@ -0,0 +1,22 @@
+import createClient, { type Middleware } from "openapi-fetch";
+import type { paths } from "./api";
+import {env} from "$env/dynamic/public"
+
+const client = createClient<paths>({ baseUrl: env.PUBLIC_API_URL,  });
+
+const authMiddleware: Middleware = {
+	async onRequest({ request }) {
+		if (typeof document === "undefined") return request; // SSR
+		
+		const token = document.cookie.match(/token=([^;]+)/)?.[1];
+		if (token)
+			request.headers.set("Authorization", `Bearer ${token}`);
+
+		return request;
+	},
+};
+
+client.use(authMiddleware);
+
+export default client;
+
